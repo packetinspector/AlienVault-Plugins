d4fae68f4a9f02c9ea7c40de0b4bf9a007fb38f6
PacketInspector <root@alienvault.alienvault>
2014-08-27 20:55:07 -0400

    Added SourceFire

---
+++ SourceFire/sourcefire.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO software_cpe VALUES ('cpe:/h:Sourcefire:Sensor:-', 'Sensor', '5.x' , 'Sourcefire DefenseCenter 5.x', 'Sourcefire', 'sourcefire:1001');
+INSERT IGNORE INTO software_cpe VALUES ('cpe:/h:Sourcefire:Defense Center:-', 'Defense Center', '5.x' , 'Sourcefire DefenseCenter 5.x', 'Sourcefire', 'sourcefire:1001');
