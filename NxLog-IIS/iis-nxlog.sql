9a4aef20a4431b0cb44b2f1e5d2b847759020659
scottymace <scottymace@gmail.com>
2016-06-17 12:09:14 -0500

    Add files via upload

--- NxLog-IIS/iis-nxlog.sql
+++ NxLog-IIS/iis-nxlog.sql
@@ -1,2 +1,1 @@
-INSERT IGNORE INTO software_cpe VALUES ('cpe:/h:Microsoft:IIS:-', 'IIS-W3C', '7.5' , 'Microsoft IIS 7.5', 'IIS-W3C', 'iis-nxlog:1502');
-INSERT IGNORE INTO plugin (id, type, name, description,product_type,vendor) VALUES (1502, 1, 'iis-nxlog', 'IIS W3C',17,'Microsoft');
+INSERT IGNORE INTO software_cpe VALUES ('cpe:/a:Microsoft:IIS-W3C:-', 'IIS-W3C', '7.5', 'Microsoft IIS-WC3 7.5 ', 'IIS-W3C', 'iis-nxlog:1502');
